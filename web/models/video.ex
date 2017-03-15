7a00ceeabf618075b18968421d398a89a29e460a
aostrovsky <anatolyostrovsky@gmail.com>
2017-03-15 15:10:09 +0200

    Chapter 9

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,14 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
+  import Ecto.Changeset
+
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
 
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -15,9 +19,33 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
+  @required_fields ~w(url title description)
+  @optional_fields ~w()
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+    |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
+    |> assoc_constraint(:category)
   end
+
+  defp slugify_title(changeset) do
+      if title = get_change(changeset, :title) do
+        put_change(changeset, :slug, slugify(title))
+      else
+        changeset
+      end
+    end
+
+  defp slugify(str) do
+      str
+      |> String.downcase()
+      |> String.replace(~r/[^\w-]+/u, "-")
+    end
+
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
 end
