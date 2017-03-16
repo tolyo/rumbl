cc1d003261c841dca30a88ab1d5623e8391a1fa9
tolyo <anatolyostrovsky@gmail.com>
2017-03-16 02:53:34 +0200

    Chapter 10

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,7 +1,6 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
-
   import Phoenix.Controller
   alias Rumbl.Router.Helpers
 
@@ -11,15 +10,30 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(Rumbl.User, user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+      user = user_id && repo.get(Rumbl.User, user_id) ->
+        put_current_user(conn, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
@@ -29,6 +43,7 @@ defmodule Rumbl.Auth do
   def login_by_username_and_pass(conn, username, given_pass, opts) do
     repo = Keyword.fetch!(opts, :repo)
     user = repo.get_by(Rumbl.User, username: username)
+
     cond do
       user && checkpw(given_pass, user.password_hash) ->
         {:ok, login(conn, user)}
@@ -48,6 +63,6 @@ defmodule Rumbl.Auth do
       |> put_flash(:error, "You must be logged in to access that page")
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
-     end
+    end
   end
 end
